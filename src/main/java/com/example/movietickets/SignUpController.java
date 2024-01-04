361eac437300760df0eb2e07937812a23f3f324d
man250001 <monizcr@oldcolony.info>
2024-01-04 09:58:52 -0500

    Allow sign-ups!

--- src/main/java/com/example/movietickets/SignUpController.java
+++ src/main/java/com/example/movietickets/SignUpController.java
@@ -1,10 +1,14 @@
 package com.example.movietickets;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @SuppressWarnings("unused")
-public class SignUpController {
+public class SignUpController implements Initializable {
 
     @FXML
     private TextField emailBox, usernameBox;
@@ -17,4 +21,22 @@ public class SignUpController {
 
     @FXML
     private Button signUpButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        signUpButton.setOnAction(actionEvent -> {
+            try {
+                DBUtils.signUpUser(actionEvent, usernameBox.getText(), passwordBox.getText(), emailBox.getText());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        logInLink.setOnAction(actionEvent -> {
+            try {
+                DBUtils.changeScene(actionEvent, "Login.fxml", "Login");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
