3de3f16f2f5b6ff8805f5a8ff11f65691f64cdcc
man250001 <monizcr@oldcolony.info>
2024-01-04 08:50:08 -0500

    Create Sign Up

---
+++ src/main/java/com/example/movietickets/SignUpController.java
@@ -0,0 +1,20 @@
+package com.example.movietickets;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+@SuppressWarnings("unused")
+public class SignUpController {
+
+    @FXML
+    private TextField emailBox, usernameBox;
+
+    @FXML
+    private Hyperlink logInLink;
+
+    @FXML
+    private PasswordField passwordBox;
+
+    @FXML
+    private Button signUpButton;
+}
