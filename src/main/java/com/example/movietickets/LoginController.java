12aea9f1e59a3e685e93bbe1f814510c1374c7f9
man250001 <monizcr@oldcolony.info>
2024-01-04 08:49:38 -0500

    Controller Skeleton

--- src/main/java/com/example/movietickets/LoginController.java
+++ src/main/java/com/example/movietickets/LoginController.java
@@ -1,5 +1,20 @@
 package com.example.movietickets;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class LoginController {
+
+    @FXML
+    private PasswordField passwordBox;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Hyperlink signUpLink;
+
+    @FXML
+    private TextField usernameBox;
 
 }
