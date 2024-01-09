a7c5ae60543e46243f409481addf4126c6602914
man250001 <monizcr@oldcolony.info>
2024-01-09 09:49:44 -0500

    Add Movie ID to record

--- src/main/java/com/example/movietickets/Movie.java
+++ src/main/java/com/example/movietickets/Movie.java
@@ -1,5 +1,5 @@
 package com.example.movietickets;
 import javafx.scene.image.Image;
 import java.sql.Date;
-public record Movie(String title, String genre, String duration, Date releaseDate, Image image) {
+public record Movie(int ID, String title, String genre, String duration, Date releaseDate, Image image) {
 }
